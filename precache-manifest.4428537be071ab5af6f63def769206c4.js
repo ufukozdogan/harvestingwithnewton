90ce4c1e43520e9088d4b790c4c63419b9a49e1c
ufukozdogan <ufukgozdogan@gmail.com>
2018-10-19 12:07:07 +0300

    Updates

---
+++ precache-manifest.4428537be071ab5af6f63def769206c4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca2d3c810ffd1506d27a2a95da6b0cd3",
+    "url": "/harvestingwithnewton/static/media/tree.ca2d3c81.svg"
+  },
+  {
+    "revision": "4f0a93aa1f625d64ccd5",
+    "url": "/harvestingwithnewton/static/css/main.b0726afb.chunk.css"
+  },
+  {
+    "revision": "7775a318fe0e941cd6f7",
+    "url": "/harvestingwithnewton/static/js/1.7775a318.chunk.js"
+  },
+  {
+    "revision": "241ebf257901bc277c83",
+    "url": "/harvestingwithnewton/static/js/runtime~main.241ebf25.js"
+  },
+  {
+    "revision": "8bdacb4b3a212b26daa1b3dfe20ef6ce",
+    "url": "/harvestingwithnewton/static/media/background.8bdacb4b.png"
+  },
+  {
+    "revision": "4f0a93aa1f625d64ccd5",
+    "url": "/harvestingwithnewton/static/js/main.4f0a93aa.chunk.js"
+  },
+  {
+    "revision": "ff7af82ff4c1cf9c993a405d315e16df",
+    "url": "/harvestingwithnewton/static/media/basket.ff7af82f.svg"
+  },
+  {
+    "revision": "5761b02f2081574d000e3771b5cb3337",
+    "url": "/harvestingwithnewton/static/media/apple.5761b02f.svg"
+  },
+  {
+    "revision": "5a720d14253ff7d8c0305813d4172ec3",
+    "url": "/harvestingwithnewton/static/media/grab.5a720d14.svg"
+  },
+  {
+    "revision": "7c48f295f9c595dcb04b81d8d5c0dac4",
+    "url": "/harvestingwithnewton/static/media/sign.7c48f295.png"
+  },
+  {
+    "revision": "32771a25073d516917e8a014f651b87f",
+    "url": "/harvestingwithnewton/index.html"
+  }
+];
